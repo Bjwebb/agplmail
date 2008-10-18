5134cc48ce8bc01cdc2c68908aa0c32aa3c315fa
Ben Webb <bjwebb@teenlug.vm.bytemark.co.uk>
2008-10-18 18:08:27 +0100

    Add installation instructions

---
+++ structure.sql
@@ -0,0 +1,27 @@
+-- SQL Table structure for AGPLMail
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `agplmail_mess`
+-- 
+
+CREATE TABLE `agplmail_mess` (
+  `msgno` mediumint(9) NOT NULL,
+  `archived` tinyint(1) NOT NULL,
+  `tags` mediumtext NOT NULL,
+  `account` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `agplmail_settings`
+-- 
+
+CREATE TABLE `agplmail_settings` (
+  `account` text NOT NULL,
+  `name` text NOT NULL,
+  `value` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
